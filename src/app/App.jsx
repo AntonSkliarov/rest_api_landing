6d6ccab4276f01856f6ea2af8fd84ca9362726ed
anton <anton@coderiver.com.ua>
2021-02-17 10:38:17 +0200

    setup sass and eslint

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.sass';
+
+export function App() {
+   return (
+     <div className="App">
+       <h1>Rest Api Page</h1>
+       <img src="/images/1.jpg" alt="First"/>
+     </div>
+   );
+ }
